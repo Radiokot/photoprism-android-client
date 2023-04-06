e8246270a6793d2800582ed10404516828625feb
Oleg Koretsky <oleg@radiokot.com.ua>
2023-04-06 21:39:36 +0300

    Make HTTP basic auth work with URL credentials

---
+++ app/src/main/java/ua/com/radiokot/photoprism/api/util/UrlBasicAuthInterceptor.kt
@@ -0,0 +1,47 @@
+package ua.com.radiokot.photoprism.api.util
+
+import okhttp3.Credentials
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * An [Interceptor] that takes [HttpUrl.username] and [HttpUrl.password]
+ * and makes an "Authorization: Basic" header from them.
+ *
+ * To avoid logging sensitive data, once handled the URL credentials are masked.
+ */
+class UrlBasicAuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val url = request.url
+        val urlUsername = url.username
+        val urlPassword = url.password
+
+        val requestToProceed =
+            if ((urlUsername.isNotEmpty() || urlPassword.isNotEmpty())
+                && request.header(AUTH_HEADER) == null
+            )
+                request.newBuilder()
+                    .header(
+                        AUTH_HEADER,
+                        Credentials.basic(urlUsername, urlPassword, Charsets.UTF_8)
+                    )
+                    .url(
+                        request.url.newBuilder()
+                            .username(URL_CREDENTIALS_MASK)
+                            .password(URL_CREDENTIALS_MASK)
+                            .build()
+                    )
+                    .build()
+            else
+                request
+
+        return chain.proceed(requestToProceed)
+    }
+
+    private companion object {
+        private const val AUTH_HEADER = "Authorization"
+        private const val URL_CREDENTIALS_MASK = "handled"
+    }
+}
