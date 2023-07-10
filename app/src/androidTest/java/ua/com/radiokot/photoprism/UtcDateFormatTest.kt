b86feb3cea88d83153b01d5132408561efe13e52
Oleg Koretsky <oleg@radiokot.com.ua>
2023-07-10 23:16:59 +0300

    Add instrumented test for UTC date formats

---
+++ app/src/androidTest/java/ua/com/radiokot/photoprism/UtcDateFormatTest.kt
@@ -0,0 +1,43 @@
+package ua.com.radiokot.photoprism
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.qualifier.named
+import org.koin.dsl.koinApplication
+import ua.com.radiokot.photoprism.di.UTC_DATE_TIME_DATE_FORMAT
+import ua.com.radiokot.photoprism.di.UTC_DATE_TIME_YEAR_DATE_FORMAT
+import ua.com.radiokot.photoprism.di.UTC_DAY_DATE_FORMAT
+import ua.com.radiokot.photoprism.di.UTC_DAY_YEAR_DATE_FORMAT
+import ua.com.radiokot.photoprism.di.UTC_MONTH_DATE_FORMAT
+import ua.com.radiokot.photoprism.di.UTC_MONTH_YEAR_DATE_FORMAT
+import ua.com.radiokot.photoprism.di.dateFormatModules
+import java.text.DateFormat
+
+@RunWith(AndroidJUnit4::class)
+class UtcDateFormatTest {
+    @Test
+    fun ensureTimeZones() {
+        val koin = koinApplication {
+            modules(dateFormatModules)
+        }.koin
+
+        listOf(
+            UTC_MONTH_DATE_FORMAT,
+            UTC_MONTH_YEAR_DATE_FORMAT,
+            UTC_DAY_DATE_FORMAT,
+            UTC_DAY_YEAR_DATE_FORMAT,
+            UTC_DATE_TIME_DATE_FORMAT,
+            UTC_DATE_TIME_YEAR_DATE_FORMAT,
+            UTC_DATE_TIME_YEAR_DATE_FORMAT,
+        ).forEach { dateFormatName ->
+            val format = koin.get<DateFormat>(named(dateFormatName))
+
+            Assert.assertTrue(
+                "$dateFormatName format must have the UTC timezone",
+                format.timeZone.id == "UTC"
+            )
+        }
+    }
+}
