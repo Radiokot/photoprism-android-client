4d94db4b77f2661b49b3bf2fdf420dce5e45c9b3
Oleg Koretsky <oleg@radiokot.com.ua>
2023-09-14 22:17:45 +0300

    Move search DI modules to a separate file

--- app/src/main/java/ua/com/radiokot/photoprism/features/gallery/di/GalleryFeatureModules.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/gallery/di/GalleryFeatureModules.kt
@@ -1,35 +1,25 @@
 package ua.com.radiokot.photoprism.features.gallery.di
 
 import android.net.Uri
-import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.scopedOf
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.qualifier._q
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import ua.com.radiokot.photoprism.BuildConfig
-import ua.com.radiokot.photoprism.db.AppDatabase
-import ua.com.radiokot.photoprism.di.APP_NO_BACKUP_PREFERENCES
 import ua.com.radiokot.photoprism.di.INTERNAL_DOWNLOADS_DIRECTORY
-import ua.com.radiokot.photoprism.di.INTERNAL_EXPORT_DIRECTORY
 import ua.com.radiokot.photoprism.di.SelfParameterHolder
 import ua.com.radiokot.photoprism.di.UTC_DAY_DATE_FORMAT
 import ua.com.radiokot.photoprism.di.UTC_DAY_YEAR_DATE_FORMAT
 import ua.com.radiokot.photoprism.di.UTC_MONTH_DATE_FORMAT
 import ua.com.radiokot.photoprism.di.UTC_MONTH_YEAR_DATE_FORMAT
 import ua.com.radiokot.photoprism.di.dateFormatModules
-import ua.com.radiokot.photoprism.di.dbModules
-import ua.com.radiokot.photoprism.di.ioModules
 import ua.com.radiokot.photoprism.env.data.model.EnvSession
 import ua.com.radiokot.photoprism.features.envconnection.di.envConnectionFeatureModules
-import ua.com.radiokot.photoprism.features.gallery.data.storage.SearchBookmarksRepository
-import ua.com.radiokot.photoprism.features.gallery.data.storage.SearchPreferences
-import ua.com.radiokot.photoprism.features.gallery.data.storage.SearchPreferencesOnPrefs
 import ua.com.radiokot.photoprism.features.gallery.data.storage.SimpleGalleryMediaRepository
 import ua.com.radiokot.photoprism.features.gallery.logic.DownloadFileUseCase
 import ua.com.radiokot.photoprism.features.gallery.logic.FileReturnIntentCreator
@@ -39,23 +29,10 @@ import ua.com.radiokot.photoprism.features.gallery.logic.MediaWebUrlFactory
 import ua.com.radiokot.photoprism.features.gallery.logic.PhotoPrismMediaDownloadUrlFactory
 import ua.com.radiokot.photoprism.features.gallery.logic.PhotoPrismMediaWebUrlFactory
 import ua.com.radiokot.photoprism.features.gallery.logic.PhotoPrismPreviewUrlFactory
-import ua.com.radiokot.photoprism.features.gallery.search.albums.data.model.Album
-import ua.com.radiokot.photoprism.features.gallery.search.albums.data.storage.AlbumsRepository
-import ua.com.radiokot.photoprism.features.gallery.search.albums.view.model.AlbumsOverviewViewModel
-import ua.com.radiokot.photoprism.features.gallery.search.albums.view.model.GallerySearchAlbumsViewModel
-import ua.com.radiokot.photoprism.features.gallery.search.logic.ExportSearchBookmarksUseCase
-import ua.com.radiokot.photoprism.features.gallery.search.logic.ImportSearchBookmarksUseCase
-import ua.com.radiokot.photoprism.features.gallery.search.logic.JsonSearchBookmarksBackup
-import ua.com.radiokot.photoprism.features.gallery.search.logic.SearchBookmarksBackup
-import ua.com.radiokot.photoprism.features.gallery.search.logic.SearchPredicates
+import ua.com.radiokot.photoprism.features.gallery.search.di.gallerySearchFeatureModules
 import ua.com.radiokot.photoprism.features.gallery.search.logic.TvDetector
 import ua.com.radiokot.photoprism.features.gallery.search.logic.TvDetectorImpl
-import ua.com.radiokot.photoprism.features.gallery.search.people.data.model.Person
-import ua.com.radiokot.photoprism.features.gallery.search.people.data.storage.PeopleRepository
-import ua.com.radiokot.photoprism.features.gallery.search.people.view.model.GallerySearchPeopleViewModel
-import ua.com.radiokot.photoprism.features.gallery.search.people.view.model.PeopleOverviewViewModel
 import ua.com.radiokot.photoprism.features.gallery.search.view.model.GallerySearchViewModel
-import ua.com.radiokot.photoprism.features.gallery.search.view.model.SearchBookmarkDialogViewModel
 import ua.com.radiokot.photoprism.features.gallery.view.model.DownloadMediaFileViewModel
 import ua.com.radiokot.photoprism.features.gallery.view.model.GalleryFastScrollViewModel
 import ua.com.radiokot.photoprism.features.gallery.view.model.GalleryViewModel
@@ -70,6 +47,7 @@ val galleryFeatureModules: List<Module> = listOf(
     module {
         includes(envConnectionFeatureModules)
         includes(dateFormatModules)
+        includes(gallerySearchFeatureModules)
 
         single {
             FileReturnIntentCreator(
@@ -111,6 +89,14 @@ val galleryFeatureModules: List<Module> = listOf(
 
             scopedOf(DownloadFileUseCase::Factory)
 
+            // Downloader must be session-scoped to have the correct
+            // HTTP client (e.g. for mTLS)
+            scopedOf(::OkHttpObservableDownloader) {
+                bind<ObservableDownloader>()
+            }
+
+            viewModelOf(::DownloadMediaFileViewModel)
+
             scoped {
                 SimpleGalleryMediaRepository.Factory(
                     photoPrismPhotosService = get(),
@@ -121,47 +107,7 @@ val galleryFeatureModules: List<Module> = listOf(
                 )
             } bind SimpleGalleryMediaRepository.Factory::class
 
-            scoped {
-                AlbumsRepository(
-                    photoPrismAlbumsService = get(),
-                    previewUrlFactory = get(),
-                    types = listOf("album", "folder"),
-                    comparator = compareByDescending(Album::isFavorite)
-                        .thenBy(Album::title)
-                )
-            } bind AlbumsRepository::class
-
-            scopedOf(::PeopleRepository)
-
-            // Downloader must be session-scoped to have the correct
-            // HTTP client (e.g. for mTLS)
-            scopedOf(::OkHttpObservableDownloader) {
-                bind<ObservableDownloader>()
-            }
-
-            viewModelOf(::DownloadMediaFileViewModel)
-
-            viewModelOf(::GallerySearchAlbumsViewModel)
-            viewModelOf(::GallerySearchPeopleViewModel)
             viewModelOf(::GallerySearchViewModel)
-
-            viewModel {
-                AlbumsOverviewViewModel(
-                    albumsRepository = get(),
-                    searchPredicate = { album: Album, query: String ->
-                        SearchPredicates.generalCondition(query, album.title)
-                    }
-                )
-            }
-
-            viewModel {
-                PeopleOverviewViewModel(
-                    peopleRepository = get(),
-                    searchPredicate = { person: Person, query: String ->
-                        SearchPredicates.generalCondition(query, person.name)
-                    }
-                )
-            }
 
             viewModel {
                 GalleryFastScrollViewModel(
@@ -185,56 +131,5 @@ val galleryFeatureModules: List<Module> = listOf(
                 )
             }
         }
-    },
-
-    module {
-        includes(dbModules)
-
-        single {
-            SearchBookmarksRepository(
-                bookmarksDbDao = get<AppDatabase>().bookmarks(),
-            )
-        } bind SearchBookmarksRepository::class
-
-        viewModelOf(::SearchBookmarkDialogViewModel)
-    },
-
-    module {
-        includes(ioModules)
-
-        single {
-            JsonSearchBookmarksBackup(jsonObjectMapper = get())
-        } bind SearchBookmarksBackup::class
-
-        factory {
-            ExportSearchBookmarksUseCase(
-                exportDir = get(named(INTERNAL_EXPORT_DIRECTORY)),
-                backupStrategy = get(),
-                fileReturnIntentCreator = get(),
-                searchBookmarksRepository = get(),
-            )
-        } bind ExportSearchBookmarksUseCase::class
-
-        factory(_q<ImportSearchBookmarksUseCaseParams>()) { params ->
-            params as ImportSearchBookmarksUseCaseParams
-
-            ImportSearchBookmarksUseCase(
-                fileUri = params.fileUri,
-                backupStrategy = get(),
-                searchBookmarksRepository = get(),
-                contentResolver = androidContext().contentResolver,
-            )
-        } bind ImportSearchBookmarksUseCase::class
-    },
-
-    module {
-        includes(ioModules)
-
-        single {
-            SearchPreferencesOnPrefs(
-                preferences = get(named(APP_NO_BACKUP_PREFERENCES)),
-                keyPrefix = "search",
-            )
-        } bind SearchPreferences::class
     },
 )
---
+++ app/src/main/java/ua/com/radiokot/photoprism/features/gallery/search/di/GallerySearchFeatureModules.kt
@@ -0,0 +1,133 @@
+package ua.com.radiokot.photoprism.features.gallery.search.di
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.scopedOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier._q
+import org.koin.core.qualifier.named
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import ua.com.radiokot.photoprism.db.AppDatabase
+import ua.com.radiokot.photoprism.di.APP_NO_BACKUP_PREFERENCES
+import ua.com.radiokot.photoprism.di.INTERNAL_EXPORT_DIRECTORY
+import ua.com.radiokot.photoprism.di.dbModules
+import ua.com.radiokot.photoprism.di.ioModules
+import ua.com.radiokot.photoprism.env.data.model.EnvSession
+import ua.com.radiokot.photoprism.features.gallery.data.storage.SearchBookmarksRepository
+import ua.com.radiokot.photoprism.features.gallery.data.storage.SearchPreferences
+import ua.com.radiokot.photoprism.features.gallery.data.storage.SearchPreferencesOnPrefs
+import ua.com.radiokot.photoprism.features.gallery.di.ImportSearchBookmarksUseCaseParams
+import ua.com.radiokot.photoprism.features.gallery.search.albums.data.model.Album
+import ua.com.radiokot.photoprism.features.gallery.search.albums.data.storage.AlbumsRepository
+import ua.com.radiokot.photoprism.features.gallery.search.albums.view.model.AlbumsOverviewViewModel
+import ua.com.radiokot.photoprism.features.gallery.search.albums.view.model.GallerySearchAlbumsViewModel
+import ua.com.radiokot.photoprism.features.gallery.search.logic.ExportSearchBookmarksUseCase
+import ua.com.radiokot.photoprism.features.gallery.search.logic.ImportSearchBookmarksUseCase
+import ua.com.radiokot.photoprism.features.gallery.search.logic.JsonSearchBookmarksBackup
+import ua.com.radiokot.photoprism.features.gallery.search.logic.SearchBookmarksBackup
+import ua.com.radiokot.photoprism.features.gallery.search.logic.SearchPredicates
+import ua.com.radiokot.photoprism.features.gallery.search.people.data.model.Person
+import ua.com.radiokot.photoprism.features.gallery.search.people.data.storage.PeopleRepository
+import ua.com.radiokot.photoprism.features.gallery.search.people.view.model.GallerySearchPeopleViewModel
+import ua.com.radiokot.photoprism.features.gallery.search.people.view.model.PeopleOverviewViewModel
+import ua.com.radiokot.photoprism.features.gallery.search.view.model.SearchBookmarkDialogViewModel
+
+val gallerySearchFeatureModules: List<Module> = listOf(
+    // Bookmarks.
+    module {
+        includes(dbModules)
+
+        single {
+            SearchBookmarksRepository(
+                bookmarksDbDao = get<AppDatabase>().bookmarks(),
+            )
+        } bind SearchBookmarksRepository::class
+
+        viewModelOf(::SearchBookmarkDialogViewModel)
+
+        // Import/export.
+        singleOf(::JsonSearchBookmarksBackup) {
+            bind<SearchBookmarksBackup>()
+        }
+
+        factory {
+            ExportSearchBookmarksUseCase(
+                exportDir = get(named(INTERNAL_EXPORT_DIRECTORY)),
+                backupStrategy = get(),
+                fileReturnIntentCreator = get(),
+                searchBookmarksRepository = get(),
+            )
+        } bind ExportSearchBookmarksUseCase::class
+
+        factory(_q<ImportSearchBookmarksUseCaseParams>()) { params ->
+            params as ImportSearchBookmarksUseCaseParams
+
+            ImportSearchBookmarksUseCase(
+                fileUri = params.fileUri,
+                backupStrategy = get(),
+                searchBookmarksRepository = get(),
+                contentResolver = androidContext().contentResolver,
+            )
+        } bind ImportSearchBookmarksUseCase::class
+    },
+
+    // Albums.
+    module {
+        scope<EnvSession> {
+            scoped {
+                AlbumsRepository(
+                    photoPrismAlbumsService = get(),
+                    previewUrlFactory = get(),
+                    types = listOf("album", "folder"),
+                    comparator = compareByDescending(Album::isFavorite)
+                        .thenBy(Album::title)
+                )
+            } bind AlbumsRepository::class
+
+            viewModelOf(::GallerySearchAlbumsViewModel)
+
+            viewModel {
+                AlbumsOverviewViewModel(
+                    albumsRepository = get(),
+                    searchPredicate = { album: Album, query: String ->
+                        SearchPredicates.generalCondition(query, album.title)
+                    }
+                )
+            }
+        }
+    },
+
+    // People.
+    module {
+        scope<EnvSession> {
+            scopedOf(::PeopleRepository)
+
+            viewModelOf(::GallerySearchPeopleViewModel)
+
+            viewModel {
+                PeopleOverviewViewModel(
+                    peopleRepository = get(),
+                    searchPredicate = { person: Person, query: String ->
+                        SearchPredicates.generalCondition(query, person.name)
+                    }
+                )
+            }
+        }
+    },
+
+    // Preferences.
+    module {
+        includes(ioModules)
+
+        single {
+            SearchPreferencesOnPrefs(
+                preferences = get(named(APP_NO_BACKUP_PREFERENCES)),
+                keyPrefix = "search",
+            )
+        } bind SearchPreferences::class
+    },
+)
