e8228ba07e9817a510ef4ba5a6b439b71c398433
Oleg Koretsky <oleg@radiokot.com.ua>
2024-02-03 16:42:53 +0200

    Add DB entity and tests for memories

---
+++ app/src/main/java/ua/com/radiokot/photoprism/features/memories/data/model/MemoryDbEntity.kt
@@ -0,0 +1,83 @@
+package ua.com.radiokot.photoprism.features.memories.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import ua.com.radiokot.photoprism.di.JsonObjectMapper
+import java.util.Date
+
+@TypeConverters(MemoryDbEntity.Converters::class)
+data class MemoryDbEntity(
+    @PrimaryKey
+    val searchQuery: String,
+    @ColumnInfo("is_seen")
+    val isSeen: Boolean,
+    @ColumnInfo("created_at_ms")
+    val createdAtMs: Long,
+    @ColumnInfo("small_thumbnail_url")
+    val smallThumbnailUrl: String,
+    @ColumnInfo("type_data")
+    val typeData: TypeData,
+) {
+    class Converters : KoinComponent {
+        private val jsonMapper: JsonObjectMapper by inject()
+
+        @TypeConverter
+        fun typeDataFromJson(value: String?): TypeData? =
+            value?.let(jsonMapper::readValue)
+
+        @TypeConverter
+        fun typeDataToJson(value: TypeData?): String? =
+            value?.let(jsonMapper::writeValueAsString)
+    }
+
+    @JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "_t",
+    )
+    @JsonSubTypes(
+        JsonSubTypes.Type(value = TypeData.ThisDayInThePast::class, name = "1"),
+    )
+    sealed class TypeData {
+
+        class ThisDayInThePast
+        @JsonCreator
+        constructor(
+            @JsonProperty("year")
+            val year: Int
+        ) : TypeData()
+    }
+
+    constructor(memory: Memory) : this(
+        searchQuery = memory.searchQuery,
+        isSeen = memory.isSeen,
+        createdAtMs = memory.createdAt.time,
+        smallThumbnailUrl = memory.smallThumbnailUrl,
+        typeData = when (memory) {
+            is Memory.ThisDayInThePast ->
+                TypeData.ThisDayInThePast(
+                    year = memory.year,
+                )
+        }
+    )
+
+    fun toMemory() = when (typeData) {
+        is TypeData.ThisDayInThePast ->
+            Memory.ThisDayInThePast(
+                year = typeData.year,
+                searchQuery = this.searchQuery,
+                isSeen = this.isSeen,
+                createdAt = Date(this.createdAtMs),
+                smallThumbnailUrl = this.smallThumbnailUrl,
+            )
+    }
+}
---
+++ app/src/test/java/ua/com/radiokot/photoprism/MemoryDbEntityTest.kt
@@ -0,0 +1,104 @@
+package ua.com.radiokot.photoprism
+
+import org.junit.Assert
+import org.junit.BeforeClass
+import org.junit.Test
+import org.koin.core.component.KoinComponent
+import org.koin.core.context.startKoin
+import ua.com.radiokot.photoprism.di.ioModules
+import ua.com.radiokot.photoprism.features.memories.data.model.Memory
+import ua.com.radiokot.photoprism.features.memories.data.model.MemoryDbEntity
+import java.util.Date
+import kotlin.test.assertIs
+
+class MemoryDbEntityTest : KoinComponent {
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun initKoin() {
+            startKoin {
+                modules(ioModules)
+            }
+        }
+    }
+
+    @Test
+    fun createFromMemorySuccessfully() {
+        val memory = Memory.ThisDayInThePast(
+            year = 2021,
+            searchQuery = "uid:1|2|3",
+            createdAt = Date(1706952860000),
+            isSeen = false,
+            smallThumbnailUrl = "http://example.com"
+        )
+
+        val entity = MemoryDbEntity(memory)
+
+        Assert.assertEquals(memory.searchQuery, entity.searchQuery)
+        Assert.assertEquals(memory.createdAt.time, entity.createdAtMs)
+        Assert.assertEquals(memory.isSeen, entity.isSeen)
+        Assert.assertEquals(memory.smallThumbnailUrl, entity.smallThumbnailUrl)
+
+        val typeData = assertIs<MemoryDbEntity.TypeData.ThisDayInThePast>(entity.typeData)
+        Assert.assertEquals(memory.year, typeData.year)
+    }
+
+    @Test
+    fun mapToMemorySuccessfully() {
+        val entity = MemoryDbEntity(
+            searchQuery = "uid:1|2|3",
+            createdAtMs = 1706952860000,
+            isSeen = false,
+            smallThumbnailUrl = "http://example.com",
+            typeData = MemoryDbEntity.TypeData.ThisDayInThePast(
+                year = 2021,
+            )
+        )
+
+        val memory = entity.toMemory()
+
+        Assert.assertEquals(entity.searchQuery, memory.searchQuery)
+        Assert.assertEquals(entity.createdAtMs, memory.createdAt.time)
+        Assert.assertEquals(entity.isSeen, memory.isSeen)
+        Assert.assertEquals(entity.smallThumbnailUrl, memory.smallThumbnailUrl)
+
+        val thisDayInThePast = assertIs<Memory.ThisDayInThePast>(memory)
+        Assert.assertEquals(
+            (entity.typeData as MemoryDbEntity.TypeData.ThisDayInThePast).year,
+            thisDayInThePast.year
+        )
+    }
+
+    @Test
+    fun convertTypeDataToJsonSuccessfully() {
+        val typeData = MemoryDbEntity.TypeData.ThisDayInThePast(
+            year = 2021,
+        )
+
+        val json = MemoryDbEntity.Converters().typeDataToJson(typeData)
+
+        Assert.assertEquals("{\"_t\":\"1\",\"year\":2021}", json)
+    }
+
+    @Test
+    fun convertTypeDataToJsonSuccessfully_IfNull() {
+        val json = MemoryDbEntity.Converters().typeDataToJson(null)
+        Assert.assertNull(json)
+    }
+
+    @Test
+    fun convertJsonToTypeDataSuccessfully() {
+        val json = "{\"_t\":\"1\",\"year\":2021}"
+
+        val typeData = MemoryDbEntity.Converters().typeDataFromJson(json)
+
+        assertIs<MemoryDbEntity.TypeData.ThisDayInThePast>(typeData)
+        Assert.assertEquals(2021, typeData.year)
+    }
+
+    @Test
+    fun convertJsonToTypeDataSuccessfully_IfNull() {
+        val typeData = MemoryDbEntity.Converters().typeDataFromJson(null)
+        Assert.assertNull(typeData)
+    }
+}
