f5171810dcde157ff726531a1cdfb09c6427663c
Oleg Koretsky <oleg@radiokot.com.ua>
2024-03-27 23:33:35 +0200

    Add key extensions activation method

--- app/src/main/java/ua/com/radiokot/photoprism/features/ext/data/storage/GalleryExtensionsStateRepository.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/ext/data/storage/GalleryExtensionsStateRepository.kt
@@ -3,7 +3,9 @@ package ua.com.radiokot.photoprism.features.ext.data.storage
 import ua.com.radiokot.photoprism.base.data.storage.ObjectPersistence
 import ua.com.radiokot.photoprism.featureflags.logic.FeatureFlags
 import ua.com.radiokot.photoprism.features.ext.data.model.ActivatedGalleryExtension
+import ua.com.radiokot.photoprism.features.ext.data.model.GalleryExtension
 import ua.com.radiokot.photoprism.features.ext.data.model.GalleryExtensionsState
+import java.util.Date
 import kotlin.properties.Delegates
 
 class GalleryExtensionsStateRepository(
@@ -43,4 +45,58 @@ class GalleryExtensionsStateRepository(
 
     override fun hasFeature(feature: FeatureFlags.Feature): Boolean =
         feature in activatedExtensionsFeatures
+
+    /**
+     * Activates extensions parsed from a key.
+     * Which of the [keyExtensions] will actually be activated depends on the current state.
+     *
+     * @return collection of actually activated extensions,
+     * which may be smaller than [keyExtensions].
+     */
+    fun activateKeyExtensions(
+        keySubject: String,
+        keyExtensions: Collection<GalleryExtension>,
+        keyExpirationDate: Date?,
+        encodedKey: String,
+    ): Collection<ActivatedGalleryExtension> {
+        if (state.primarySubject != null) {
+            require(keySubject == state.primarySubject) {
+                "The key subject must match the current primary subject: ${state.primarySubject}"
+            }
+        }
+
+        val alreadyActivatedExtensions = state.activatedExtensions.toMutableList()
+        val newActivatedExtensions = mutableListOf<ActivatedGalleryExtension>()
+
+        keyExtensions.forEach { keyExtension ->
+            val alreadyActivatedExtension = alreadyActivatedExtensions
+                .find { it.type == keyExtension }
+
+            // Activate the extension if it is new (not already activated),
+            // or if it prolongs the already activated one.
+            if (alreadyActivatedExtension == null
+                || alreadyActivatedExtension.expiresAt != null
+                && (keyExpirationDate == null || keyExpirationDate > alreadyActivatedExtension.expiresAt)
+            ) {
+                newActivatedExtensions.add(
+                    ActivatedGalleryExtension(
+                        type = keyExtension,
+                        key = encodedKey,
+                        expiresAt = keyExpirationDate,
+                    )
+                )
+
+                if (alreadyActivatedExtension != null) {
+                    alreadyActivatedExtensions.remove(alreadyActivatedExtension)
+                }
+            }
+        }
+
+        _state = state.copy(
+            primarySubject = keySubject,
+            activatedExtensions = alreadyActivatedExtensions + newActivatedExtensions,
+        )
+
+        return newActivatedExtensions
+    }
 }
--- app/src/test/java/ua/com/radiokot/photoprism/GalleryExtensionsStateRepositoryTest.kt
+++ app/src/test/java/ua/com/radiokot/photoprism/GalleryExtensionsStateRepositoryTest.kt
@@ -108,6 +108,197 @@ class GalleryExtensionsStateRepositoryTest {
         Assert.assertEquals(setOf("keyA"), state.activatedKeys)
     }
 
+    @Test
+    fun activateKeyExtensionsSuccessfully_IfAllNew() {
+        val persistence = SimpleStatePersistence(
+            state = null
+        )
+        val repository = GalleryExtensionsStateRepository(
+            statePersistence = persistence,
+        )
+
+        val keySubject = "test@local"
+        val keyExtensions = setOf(GalleryExtension.TEST)
+        val encodedKey = "ek"
+
+        val activatedExtensions = repository.activateKeyExtensions(
+            keySubject = keySubject,
+            keyExtensions = keyExtensions,
+            encodedKey = encodedKey,
+            keyExpirationDate = null,
+        )
+
+        Assert.assertEquals(1, activatedExtensions.size)
+        Assert.assertEquals(keyExtensions.first(), activatedExtensions.first().type)
+        Assert.assertEquals(keySubject, repository.state.primarySubject)
+        Assert.assertEquals(encodedKey, activatedExtensions.first().key)
+        Assert.assertNull(activatedExtensions.first().expiresAt)
+        Assert.assertArrayEquals(activatedExtensions.toTypedArray(), repository.state.activatedExtensions.toTypedArray())
+    }
+
+    @Test
+    fun activateKeyExtensionsSuccessfully_IfSomeNew() {
+        val persistence = SimpleStatePersistence(
+            state = GalleryExtensionsState(
+                activatedExtensions = listOf(
+                    ActivatedGalleryExtension(
+                        type = GalleryExtension.MEMORIES,
+                        key = "keyA",
+                        expiresAt = null,
+                    ),
+                ),
+                primarySubject = "test@local"
+            )
+        )
+        val repository = GalleryExtensionsStateRepository(
+            statePersistence = persistence,
+        )
+
+        val keySubject = "test@local"
+        val keyExtensions = setOf(
+            GalleryExtension.TEST,
+            GalleryExtension.MEMORIES
+        )
+        val encodedKey = "ek"
+
+        val activatedExtensions = repository.activateKeyExtensions(
+            keySubject = keySubject,
+            keyExtensions = keyExtensions,
+            encodedKey = encodedKey,
+            keyExpirationDate = null,
+        )
+
+        Assert.assertEquals(1, activatedExtensions.size)
+        Assert.assertEquals(GalleryExtension.TEST, activatedExtensions.first().type)
+        Assert.assertEquals(keySubject, repository.state.primarySubject)
+        Assert.assertEquals(encodedKey, activatedExtensions.first().key)
+        Assert.assertEquals("keyA", repository.state.activatedExtensions.first().key)
+    }
+
+    @Test
+    fun activateNoKeyExtensions_IfNoneNew() {
+        val persistence = SimpleStatePersistence(
+            state = GalleryExtensionsState(
+                activatedExtensions = listOf(
+                    ActivatedGalleryExtension(
+                        type = GalleryExtension.MEMORIES,
+                        key = "keyA",
+                        expiresAt = null,
+                    ),
+                ),
+                primarySubject = "test@local"
+            )
+        )
+        val repository = GalleryExtensionsStateRepository(
+            statePersistence = persistence,
+        )
+
+        val keySubject = "test@local"
+        val keyExtensions = setOf(
+            GalleryExtension.MEMORIES
+        )
+        val encodedKey = "ek"
+
+        val activatedExtensions = repository.activateKeyExtensions(
+            keySubject = keySubject,
+            keyExtensions = keyExtensions,
+            encodedKey = encodedKey,
+            keyExpirationDate = null,
+        )
+
+        Assert.assertEquals(0, activatedExtensions.size)
+        Assert.assertEquals("keyA", repository.state.activatedExtensions.first().key)
+    }
+
+    @Test
+    fun activateKeyExtensionsSuccessfully_IfProlongingAlreadyActivated() {
+        val persistence = SimpleStatePersistence(
+            state = GalleryExtensionsState(
+                activatedExtensions = listOf(
+                    ActivatedGalleryExtension(
+                        type = GalleryExtension.MEMORIES,
+                        key = "keyA",
+                        expiresAt = Date(System.currentTimeMillis() + 10000),
+                    ),
+                    ActivatedGalleryExtension(
+                        type = GalleryExtension.TEST,
+                        key = "keyB",
+                        expiresAt = Date(System.currentTimeMillis() + 6000),
+                    ),
+                ),
+                primarySubject = "test@local"
+            )
+        )
+        val repository = GalleryExtensionsStateRepository(
+            statePersistence = persistence,
+        )
+
+        val keySubject = "test@local"
+        val keyExtensions = setOf(
+            GalleryExtension.TEST,
+            GalleryExtension.MEMORIES
+        )
+        val encodedKey = "ek"
+        val keyExpirationDate = Date(System.currentTimeMillis() + 30000)
+
+        val activatedExtensions = repository.activateKeyExtensions(
+            keySubject = keySubject,
+            keyExtensions = keyExtensions,
+            encodedKey = encodedKey,
+            keyExpirationDate = keyExpirationDate,
+        )
+
+        Assert.assertEquals(2, activatedExtensions.size)
+        Assert.assertEquals(encodedKey, repository.state.activatedExtensions[0].key)
+        Assert.assertEquals(keyExpirationDate, repository.state.activatedExtensions[0].expiresAt)
+        Assert.assertEquals(encodedKey, repository.state.activatedExtensions[1].key)
+        Assert.assertEquals(keyExpirationDate, repository.state.activatedExtensions[1].expiresAt)
+    }
+
+    @Test
+    fun activateKeyExtensionsSuccessfully_IfProlongingAlreadyActivatedForever() {
+        val persistence = SimpleStatePersistence(
+            state = GalleryExtensionsState(
+                activatedExtensions = listOf(
+                    ActivatedGalleryExtension(
+                        type = GalleryExtension.MEMORIES,
+                        key = "keyA",
+                        expiresAt = Date(System.currentTimeMillis() + 10000),
+                    ),
+                    ActivatedGalleryExtension(
+                        type = GalleryExtension.TEST,
+                        key = "keyB",
+                        expiresAt = Date(System.currentTimeMillis() + 6000),
+                    ),
+                ),
+                primarySubject = "test@local"
+            )
+        )
+        val repository = GalleryExtensionsStateRepository(
+            statePersistence = persistence,
+        )
+
+        val keySubject = "test@local"
+        val keyExtensions = setOf(
+            GalleryExtension.TEST,
+            GalleryExtension.MEMORIES
+        )
+        val encodedKey = "ek"
+
+        val activatedExtensions = repository.activateKeyExtensions(
+            keySubject = keySubject,
+            keyExtensions = keyExtensions,
+            encodedKey = encodedKey,
+            keyExpirationDate = null,
+        )
+
+        Assert.assertEquals(2, activatedExtensions.size)
+        Assert.assertEquals(encodedKey, repository.state.activatedExtensions[0].key)
+        Assert.assertNull(repository.state.activatedExtensions[0].expiresAt)
+        Assert.assertEquals(encodedKey, repository.state.activatedExtensions[1].key)
+        Assert.assertNull(repository.state.activatedExtensions[1].expiresAt)
+    }
+
     private class SimpleStatePersistence(
         private var state: GalleryExtensionsState?,
     ) : ObjectPersistence<GalleryExtensionsState> {
