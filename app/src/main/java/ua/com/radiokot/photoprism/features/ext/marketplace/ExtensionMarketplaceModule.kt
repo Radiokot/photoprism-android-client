15ffe35a65ff7da3f299f3697d7a4c8b658f0aeb
Oleg Koretsky <oleg@radiokot.com.ua>
2024-05-04 18:50:44 +0300

    Mock the marketplace service

--- app/src/main/java/ua/com/radiokot/photoprism/features/ext/marketplace/ExtensionMarketplaceModule.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/ext/marketplace/ExtensionMarketplaceModule.kt
@@ -1,5 +1,6 @@
 package ua.com.radiokot.photoprism.features.ext.marketplace
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
@@ -12,6 +13,7 @@ import ua.com.radiokot.photoprism.di.EnvRetrofitParams
 import ua.com.radiokot.photoprism.di.localeModule
 import ua.com.radiokot.photoprism.di.retrofitApiModule
 import ua.com.radiokot.photoprism.features.ext.marketplace.api.FeatureMarketplaceService
+import ua.com.radiokot.photoprism.features.ext.marketplace.api.model.FeaturesOnSaleResponse
 import ua.com.radiokot.photoprism.features.ext.marketplace.data.storage.GalleryExtensionsOnSaleRepository
 import ua.com.radiokot.photoprism.features.ext.marketplace.view.model.GalleryExtensionMarketplaceViewModel
 import java.text.NumberFormat
@@ -24,13 +26,47 @@ val extensionMarketplaceModule = module {
         localeModule,
     )
 
+//    single {
+//        get<Retrofit>(_q<EnvRetrofitParams>()) {
+//            EnvRetrofitParams(
+//                apiUrl = "http://10.0.0.125:8041".toHttpUrl(),
+//                httpClient = get(),
+//            )
+//        }.create(FeatureMarketplaceService::class.java)
+//    } bind FeatureMarketplaceService::class
+
     single {
-        get<Retrofit>(_q<EnvRetrofitParams>()) {
-            EnvRetrofitParams(
-                apiUrl = "http://10.0.0.125:8041".toHttpUrl(),
-                httpClient = get(),
-            )
-        }.create(FeatureMarketplaceService::class.java)
+        object : FeatureMarketplaceService {
+            override fun getFeaturesOnSale(): FeaturesOnSaleResponse {
+                Thread.sleep(1000)
+
+                return get<ObjectMapper>().readValue(
+                    """
+                        {
+                            "data": [
+                                {
+                                    "type": "features",
+                                    "id": "0",
+                                    "attributes": {
+                                        "name": "The feature",
+                                        "price": "2"
+                                    }
+                                },
+                                {
+                                    "type": "features",
+                                    "id": "1",
+                                    "attributes": {
+                                        "name": "Yet another extension",
+                                        "price": "25"
+                                    }
+                                }
+                            ]
+                        }
+                    """.trimIndent(),
+                    FeaturesOnSaleResponse::class.java
+                )
+            }
+        }
     } bind FeatureMarketplaceService::class
 
     singleOf(::GalleryExtensionsOnSaleRepository)
