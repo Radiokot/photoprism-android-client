5d369f73a15d2a6ad7475c08ffd4ab951bfa1bdd
Oleg Koretsky <oleg@radiokot.com.ua>
2024-06-02 21:37:07 +0300

    Move help email address to app properties

--- app/src/main/java/ua/com/radiokot/photoprism/features/welcome/view/WelcomeActivity.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/welcome/view/WelcomeActivity.kt
@@ -3,10 +3,13 @@ package ua.com.radiokot.photoprism.features.welcome.view
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.core.text.HtmlCompat
 import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.android.inject
+import ua.com.radiokot.photoprism.R
 import ua.com.radiokot.photoprism.base.view.BaseActivity
 import ua.com.radiokot.photoprism.databinding.ActivityWelcomeBinding
+import ua.com.radiokot.photoprism.extension.checkNotNull
 import ua.com.radiokot.photoprism.extension.tryOrNull
 import ua.com.radiokot.photoprism.features.welcome.data.storage.WelcomeScreenPreferences
 import ua.com.radiokot.photoprism.util.InternalLinkMovementMethod
@@ -15,6 +18,9 @@ class WelcomeActivity : BaseActivity() {
     private lateinit var view: ActivityWelcomeBinding
 
     private val welcomeScreenPreferences: WelcomeScreenPreferences by inject()
+    private val helpEmailAddress: String = getKoin()
+        .getProperty<String>("helpEmailAddress")
+        .checkNotNull { "Missing help email address" }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +34,14 @@ class WelcomeActivity : BaseActivity() {
     }
 
     private fun initText() {
+        view.mainTextView.text = HtmlCompat.fromHtml(
+            getString(
+                R.string.template_welcome_text,
+                helpEmailAddress,
+            ),
+            HtmlCompat.FROM_HTML_MODE_COMPACT
+        )
+
         view.mainTextView.movementMethod = InternalLinkMovementMethod { url ->
             when (url) {
                 "#issue" -> {
