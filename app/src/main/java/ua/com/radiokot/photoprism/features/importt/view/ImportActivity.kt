b3dabeb940cc97cb2c6d19d8b6c0caacaf3d77e7
Oleg Koretsky <oleg@radiokot.com.ua>
2024-06-21 22:25:02 +0300

    Show pretty summary on the import screen

--- app/src/main/java/ua/com/radiokot/photoprism/features/importt/view/ImportActivity.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/importt/view/ImportActivity.kt
@@ -1,25 +1,32 @@
 package ua.com.radiokot.photoprism.features.importt.view
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.registerForActivityResult
+import androidx.annotation.StringRes
 import androidx.core.net.toUri
+import androidx.core.view.isVisible
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
+import ua.com.radiokot.photoprism.R
 import ua.com.radiokot.photoprism.base.view.BaseActivity
 import ua.com.radiokot.photoprism.databinding.ActivityImportBinding
 import ua.com.radiokot.photoprism.extension.kLogger
 import ua.com.radiokot.photoprism.features.importt.logic.ImportFilesWorker
 import ua.com.radiokot.photoprism.features.importt.logic.ParseImportIntentUseCase
+import ua.com.radiokot.photoprism.features.importt.model.ImportableFile
 
 class ImportActivity : BaseActivity() {
     private val log = kLogger("ImportActivity")
@@ -53,16 +60,18 @@ class ImportActivity : BaseActivity() {
 
         val files = parseImportIntentUseCaseFactory.get(intent).invoke()
 
-        view.debugTextView.text =
-            """
-                    Action: ${intent.action}
-                    
-                    Describe: ${
-                files.mapIndexed { index, file ->
-                    "#${index} $file"
-                }
-            }
-                """.trimIndent()
+        addSummaryItem(
+            "https://heh",
+            R.string.library_root_url
+        )
+        addSummaryItem(
+            content = getString(
+                R.string.template_import_files_size,
+                resources.getQuantityString(R.plurals.files, files.size, files.size),
+                files.sumOf(ImportableFile::size).toDouble() / (1024 * 1024)
+            ),
+            R.string.import_to_be_uploaded
+        )
 
         view.primaryButton.setOnClickListener {
             // Allow reading the URIs after the activity is finished.
@@ -93,6 +102,26 @@ class ImportActivity : BaseActivity() {
         view.cancelButton.setOnClickListener {
             finish()
         }
+    }
+
+    @SuppressLint("PrivateResource")
+    private fun addSummaryItem(
+        content: String,
+        @StringRes
+        summary: Int,
+    ) {
+        val itemLayout =
+            layoutInflater.inflate(
+                androidx.preference.R.layout.preference_material,
+                view.summaryItemsLayout,
+                false
+            )
+        itemLayout.apply {
+            findViewById<ViewGroup>(androidx.preference.R.id.icon_frame).isVisible = false
+            findViewById<TextView>(android.R.id.title).text = content
+            findViewById<TextView>(android.R.id.summary).setText(summary)
+        }
+        view.summaryItemsLayout.addView(itemLayout)
     }
 
     private fun onMediaLocationPermissionResult(isGranted: Boolean) {
