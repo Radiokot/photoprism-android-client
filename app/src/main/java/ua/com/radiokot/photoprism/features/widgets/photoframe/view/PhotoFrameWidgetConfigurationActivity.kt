76f7bfba998286f3776bc0666565a4eeecb69ada
Oleg Koretsky <oleg@radiokot.com.ua>
2024-07-27 19:58:44 +0300

    Add shape preview to the widget config screen

--- app/src/main/java/ua/com/radiokot/photoprism/features/widgets/photoframe/view/PhotoFrameWidgetConfigurationActivity.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/widgets/photoframe/view/PhotoFrameWidgetConfigurationActivity.kt
@@ -4,19 +4,23 @@ import android.app.Activity
 import android.appwidget.AppWidgetManager
 import android.content.Intent
 import android.os.Bundle
+import com.squareup.picasso.Picasso
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import org.koin.android.ext.android.inject
+import ua.com.radiokot.photoprism.R
 import ua.com.radiokot.photoprism.base.view.BaseActivity
 import ua.com.radiokot.photoprism.databinding.ActivityPhotoFrameWidgetConfigurationBinding
 import ua.com.radiokot.photoprism.extension.autoDispose
 import ua.com.radiokot.photoprism.features.widgets.photoframe.logic.ReloadPhotoFrameWidgetPhotoUseCase
 import ua.com.radiokot.photoprism.features.widgets.photoframe.logic.UpdatePhotoFrameWidgetPhotoUseCase
+import ua.com.radiokot.photoprism.util.images.ImageTransformations
 
 class PhotoFrameWidgetConfigurationActivity : BaseActivity() {
 
     private lateinit var view: ActivityPhotoFrameWidgetConfigurationBinding
     private val updatePhotoFrameWidgetPhotoUseCase: UpdatePhotoFrameWidgetPhotoUseCase by inject()
     private val reloadPhotoFrameWidgetPhotoUseCase: ReloadPhotoFrameWidgetPhotoUseCase by inject()
+    private val picasso: Picasso by inject()
     private val appWidgetId: Int by lazy {
         intent.getIntExtra(
             AppWidgetManager.EXTRA_APPWIDGET_ID,
@@ -39,7 +43,7 @@ class PhotoFrameWidgetConfigurationActivity : BaseActivity() {
         view = ActivityPhotoFrameWidgetConfigurationBinding.inflate(layoutInflater)
         setContentView(view.root)
 
-        view.okButton.setOnClickListener {
+        view.doneButton.setOnClickListener {
             updatePhotoFrameWidgetPhotoUseCase
                 .invoke(
                     widgetId = appWidgetId,
@@ -64,5 +68,78 @@ class PhotoFrameWidgetConfigurationActivity : BaseActivity() {
 
         setSupportActionBar(view.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initShapes()
+    }
+
+    private fun initShapes() {
+        val image = R.drawable.sample_image
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(
+                ImageTransformations.roundedCorners(
+                    cornerRadiusDp = 8,
+                    context = this,
+                )
+            )
+            .into(view.roundedCornersImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.fufa(this))
+            .into(view.fufaImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.sasha(this))
+            .into(view.sashaImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.leaf(this))
+            .into(view.leafImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.buba(this))
+            .into(view.bubaImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.heart(this))
+            .into(view.heartImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .centerCrop()
+            .transform(ImageTransformations.vSauce)
+            .into(view.vsauceImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.nona(this))
+            .into(view.nonaImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .transform(ImageTransformations.gear(this))
+            .into(view.gearImageView)
+
+        picasso
+            .load(image)
+            .fit()
+            .centerCrop()
+            .transform(ImageTransformations.hSauce)
+            .into(view.hsauceImageView)
     }
 }
