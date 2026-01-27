90c75dca17ae55ad025a26450c652f97c4ffeee9
Oleg Koretsky <oleg@radiokot.com.ua>
2026-01-27 19:09:23 +0200

    Initially show individual photos on the map

--- app/src/main/java/ua/com/radiokot/photoprism/api/geo/service/PhotoPrismGeoService.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/api/geo/service/PhotoPrismGeoService.kt
@@ -7,6 +7,25 @@ import java.io.IOException
 
 interface PhotoPrismGeoService {
 
+    /**
+     * @return GeoJSON with Point for each photo
+     * ```
+     *     {
+     *       "id": "9",
+     *       "type": "Feature",
+     *       "geometry": {
+     *         "type": "Point",
+     *         "coordinates": [-2.59085833333333, 42.5683033333333]
+     *       },
+     *       "properties": {
+     *         "Hash": "4bc82c3ea5aaa323aea801fe0125b554af8e49af",
+     *         "TakenAt": "2012-08-27T12:40:25Z",
+     *         "Title": "Bodegas Ysios Winery / Laguardia / Spain",
+     *         "UID": "pt986fden7x4i71c"
+     *       }
+     *     },
+     * ```
+     */
     @kotlin.jvm.Throws(IOException::class)
     @Headers("Accept: application/json")
     @GET("v1/geo")
--- app/src/main/java/ua/com/radiokot/photoprism/features/map/view/MapActivity.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/map/view/MapActivity.kt
@@ -1,18 +1,35 @@
 package ua.com.radiokot.photoprism.features.map.view
 
+import android.graphics.RectF
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.toColorInt
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.squareup.picasso.Picasso
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.maplibre.android.MapLibre
 import org.maplibre.android.camera.CameraPosition
+import org.maplibre.android.geometry.LatLngBounds
+import org.maplibre.android.maps.MapLibreMap
+import org.maplibre.android.maps.Style
 import org.maplibre.android.style.expressions.Expression
 import org.maplibre.android.style.layers.CircleLayer
 import org.maplibre.android.style.layers.PropertyFactory
+import org.maplibre.android.style.layers.SymbolLayer
+import ua.com.radiokot.photoprism.R
 import ua.com.radiokot.photoprism.base.view.BaseActivity
 import ua.com.radiokot.photoprism.databinding.ActivityMapBinding
+import ua.com.radiokot.photoprism.extension.autoDispose
+import ua.com.radiokot.photoprism.extension.intoSingle
 import ua.com.radiokot.photoprism.extension.kLogger
+import ua.com.radiokot.photoprism.features.gallery.logic.PhotoPrismMediaPreviewUrlFactory
+import ua.com.radiokot.photoprism.util.images.ImageTransformations
 
 class MapActivity : BaseActivity() {
 
@@ -22,6 +39,9 @@ class MapActivity : BaseActivity() {
     private val windowInsetsController: WindowInsetsControllerCompat by lazy {
         WindowInsetsControllerCompat(window, window.decorView)
     }
+    private val picasso by inject<Picasso>()
+    private val previewUrlFactory by inject<PhotoPrismMediaPreviewUrlFactory>()
+    private val thumbnailSizePx = 150
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +68,8 @@ class MapActivity : BaseActivity() {
     }
 
     private fun initMap() {
+        val thumbnailSizePx = 150
+
         view.map.getMapAsync { map ->
             map.setStyle("https://cdn.photoprism.app/maps/default.json")
             map.cameraPosition = CameraPosition.DEFAULT
@@ -60,25 +82,43 @@ class MapActivity : BaseActivity() {
                         // TODO: Set camera position corresponding to the source bounding box.
                     }
 
-                style.addLayer(
-                    CircleLayer("pp-photo-circles", MapViewModel.SOURCE_ID).apply {
-                        setProperties(
+                style.addImage(
+                    "placeholder",
+                    ContextCompat.getDrawable(this, R.drawable.image_placeholder_circle)!!
+                        .toBitmap(
+                            width = thumbnailSizePx,
+                            height = thumbnailSizePx,
+                        )
+                )
+
+                val clusterLayer =
+                    CircleLayer("pp-cluster-circles", MapViewModel.SOURCE_ID)
+                        .withProperties(
                             PropertyFactory.circleColor("#FF9800".toColorInt()),
                             PropertyFactory.circleRadius(10.0f),
                         )
-                        setFilter(Expression.eq(Expression.get("cluster"), true))
-                    }
-                )
+                        .withFilter(Expression.has("point_count"))
+                        .also(style::addLayer)
 
-                style.addLayer(
-                    CircleLayer("unclustered-points", MapViewModel.SOURCE_ID).apply {
-                        setProperties(
-                            PropertyFactory.circleColor("#FF0000".toColorInt()),
-                            PropertyFactory.circleRadius(10.0f),
+                val photoLayer =
+                    SymbolLayer("pp-photos", MapViewModel.SOURCE_ID)
+                        .withProperties(
+                            PropertyFactory.iconImage(
+                                Expression.coalesce(
+                                    Expression.image(Expression.get("Hash")),
+                                    Expression.image(Expression.literal("placeholder"))
+                                )
+                            ),
+                            PropertyFactory.iconSize(1f),
+                            PropertyFactory.iconAllowOverlap(true),
+                            PropertyFactory.iconIgnorePlacement(true)
                         )
-                        setFilter(Expression.neq(Expression.get("cluster"), true))
-                    }
-                )
+                        .withFilter(Expression.not(Expression.has("point_count")))
+                        .also(style::addLayer)
+
+                map.addOnCameraIdleListener {
+                    updatePhotoThumbnails(map, style, photoLayer)
+                }
 
                 log.debug {
                     "initMap(): style_initialized"
@@ -89,6 +129,69 @@ class MapActivity : BaseActivity() {
                 "initMap(): map_initialized"
             }
         }
+    }
+
+    private var photoThumbnailsUpdateDisposable: Disposable? = null
+    private fun updatePhotoThumbnails(
+        map: MapLibreMap,
+        style: Style,
+        photoLayer: SymbolLayer,
+    ) {
+        photoThumbnailsUpdateDisposable?.dispose()
+
+        val visiblePhotoFeatures = map.queryRenderedFeatures(
+            map.projection.visibleRegion.latLngBounds.toRectF(map),
+            photoLayer.id,
+        )
+
+        log.debug {
+            "updatePhotoThumbnails(): updating:" +
+                    "\nvisiblePhotoFeatures=${visiblePhotoFeatures.size}"
+        }
+
+        val visibleThumbnailHashes =
+            visiblePhotoFeatures
+                .mapTo(mutableSetOf()) { feature ->
+                    feature.getProperty("Hash").asString
+                }
+
+        photoThumbnailsUpdateDisposable =
+            visibleThumbnailHashes
+                .filter { style.getImage(it) == null }
+                .map { thumbnailHash ->
+                    picasso
+                        .load(
+                            previewUrlFactory.getThumbnailUrl(
+                                thumbnailHash = thumbnailHash,
+                                sizePx = thumbnailSizePx,
+                            )
+                        )
+                        .resize(thumbnailSizePx, thumbnailSizePx)
+                        .transform(ImageTransformations.circle)
+                        .intoSingle()
+                        .doOnSuccess { bitmap ->
+                            style.addImage(thumbnailHash, bitmap)
+                            photoLayer.setProperties(
+                                PropertyFactory.iconSize(
+                                    photoLayer.iconSize.value.let { currentValue ->
+                                        if (currentValue > 1f)
+                                            currentValue - 0.0001f
+                                        else
+                                            currentValue + 0.0001f
+                                    }
+                                )
+                            )
+                        }
+                        .ignoreElement()
+                }
+                .let(Completable::concatDelayError)
+                .doOnEvent {
+                    log.debug {
+                        "updatePhotoThumbnails(): updated"
+                    }
+                }
+                .subscribeBy()
+                .autoDispose(this)
     }
 
     override fun onStart() {
@@ -124,5 +227,12 @@ class MapActivity : BaseActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         view.map.onSaveInstanceState(outState)
+    }
+
+    private fun LatLngBounds.toRectF(map: MapLibreMap): RectF {
+        val projection = map.projection
+        val ne = projection.toScreenLocation(northEast)
+        val sw = projection.toScreenLocation(southWest)
+        return RectF(sw.x, ne.y, ne.x, sw.y)
     }
 }
