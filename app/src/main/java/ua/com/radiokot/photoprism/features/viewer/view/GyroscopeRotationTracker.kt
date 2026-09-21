7a089e95d8a4592924695bdfb266175f6fb5e43d
Oleg Koretsky <oleg@radiokot.com.ua>
2026-09-21 22:00:18 +0300

    Make gyro update rate match the FPS

--- app/src/main/java/ua/com/radiokot/photoprism/features/viewer/view/GyroscopeRotationTracker.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/viewer/view/GyroscopeRotationTracker.kt
@@ -7,7 +7,10 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.view.Surface
 import android.view.WindowManager
-import kotlin.math.abs
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.concurrent.TimeUnit
+import kotlin.math.roundToLong
 
 class GyroscopeRotationTracker(
     context: Context,
@@ -21,92 +24,105 @@ class GyroscopeRotationTracker(
     private val gyroscope =
         sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
 
-    private var lastEventNs: Long = 0L
-    private val noiseThreshold = 0.005f
+    @Suppress("DEPRECATION")
+    private val callbackPeriodMs =
+        (1000f / windowManager.defaultDisplay.refreshRate).roundToLong()
+    private var callbackTimer: Disposable? = null
+    private var lastCallbackMs = -1L
 
     var isRunning: Boolean = false
         private set
 
+    private var speedXRadS = Float.NaN
+    private var speedYRadS = Float.NaN
+    private var speedZRadS = Float.NaN
+
     fun start() {
-        if (gyroscope == null) {
+        if (isRunning || gyroscope == null) {
             return
         }
 
-        lastEventNs = 0L
         isRunning = true
+        speedXRadS = Float.NaN
+        speedYRadS = Float.NaN
+        speedZRadS = Float.NaN
 
         sensorManager.registerListener(
             this,
             gyroscope,
             SensorManager.SENSOR_DELAY_GAME,
         )
+
+        callbackTimer = createCallbackTimer().subscribe()
     }
+
+    @Suppress("DEPRECATION", "UnnecessaryVariable")
+    private fun createCallbackTimer(
+
+    ) = Observable
+        .interval(callbackPeriodMs, TimeUnit.MILLISECONDS)
+        .map { System.currentTimeMillis() }
+        .doOnNext { nowMs ->
+
+            if (lastCallbackMs == -1L
+                || speedXRadS.isNaN()
+                || speedYRadS.isNaN()
+                || speedZRadS.isNaN()
+            ) {
+                lastCallbackMs = nowMs
+                return@doOnNext
+            }
+
+            val dt = nowMs - lastCallbackMs
+            lastCallbackMs = nowMs
+
+            val dx = speedXRadS * dt / 1000f
+            val dy = speedYRadS * dt / 1000f
+            val dz = speedZRadS * dt / 1000f
+
+            val deltaRoll = dz
+            val deltaPitch: Float
+            val deltaYaw: Float
+            val displayRotation = windowManager.defaultDisplay.rotation
+
+            when (displayRotation) {
+                Surface.ROTATION_90 -> {
+                    deltaPitch = -dy
+                    deltaYaw = dx
+                }
+
+                Surface.ROTATION_270 -> {
+                    deltaPitch = dy
+                    deltaYaw = -dx
+                }
+
+                else -> {
+                    // Yes.
+                    deltaPitch = dx
+                    deltaYaw = dy
+                }
+            }
+
+            onRotation(
+                deltaPitchRad = deltaPitch,
+                deltaRollRad = deltaRoll,
+                deltaYawRad = deltaYaw,
+                displayRotation = displayRotation,
+            )
+        }
 
     fun stop() {
-        isRunning = false
+        callbackTimer?.dispose()
         sensorManager.unregisterListener(this)
+        isRunning = false
     }
 
-    @Suppress("UnnecessaryVariable", "DEPRECATION")
     override fun onSensorChanged(
         event: SensorEvent,
     ) {
-        if (lastEventNs == 0L) {
-            lastEventNs = event.timestamp
-            return
-        }
-
-        // Radians per second.
-        var (gyroX, gyroY, gyroZ) = event.values
-
-        if (abs(gyroX) < noiseThreshold) {
-            gyroX = 0f
-        }
-
-        if (abs(gyroY) < noiseThreshold) {
-            gyroY = 0f
-        }
-
-        if (abs(gyroZ) < noiseThreshold) {
-            gyroZ = 0f
-        }
-
-        val dt = (event.timestamp - lastEventNs) / 1000000000f
-        lastEventNs = event.timestamp
-
-        val dx = gyroX * dt
-        val dy = gyroY * dt
-        val dz = gyroZ * dt
-
-        val deltaRoll = dz
-        val deltaPitch: Float
-        val deltaYaw: Float
-        val displayRotation = windowManager.defaultDisplay.rotation
-
-        when (displayRotation) {
-            Surface.ROTATION_90 -> {
-                deltaPitch = -dy
-                deltaYaw = dx
-            }
-
-            Surface.ROTATION_270 -> {
-                deltaPitch = dy
-                deltaYaw = -dx
-            }
-
-            else -> {
-                // Yes.
-                deltaPitch = dx
-                deltaYaw = dy
-            }
-        }
-
-        onRotation(
-            deltaPitchRad = deltaPitch,
-            deltaRollRad = deltaRoll,
-            deltaYawRad = deltaYaw,
-            displayRotation = displayRotation,
-        )
+        speedXRadS = event.values[0]
+        speedYRadS = event.values[1]
+        speedZRadS = event.values[2]
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
